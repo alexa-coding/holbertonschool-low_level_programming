85508505df627c4e2968580763e263e32937be97
alexa-coding <alexasoucy123@gmail.com>
2018-06-20 20:56:29 +0000

    prints string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - Prints "with proper grammar, but the outcome is a piece of art,"
+* Return: an instruction not to give any value as output by the main function
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
