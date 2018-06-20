6bc8647f31c103ddbd08f337afa759ae4ed9b66c
alexa-coding <alexasoucy123@gmail.com>
2018-06-20 20:00:29 +0000

    added comments and ()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main()
+/**
+* main - Prints "Programming is like building a miltilingual puzzle
+* Return: Instructs not to give any value as output by the main function
+*/
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0); 
 }
