ad95c28ffc0a0af672f77a928273aa4608a38a16
alexa-coding <alexasoucy123@gmail.com>
2018-06-20 23:50:08 +0000

    print string with write()

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+* main - "Prints and that piece of art is useful" - Dora Korpar, 2015-10-19"
+* Return: prints error message to stderr (1)
+*/
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
