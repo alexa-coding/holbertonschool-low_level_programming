a43e673ed17131fb207ff6076207b87d3c7f4316
alexa-coding <alexasoucy123@gmail.com>
2018-06-20 20:10:31 +0000

    removed new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,6 @@
 * main - Prints "Programming is like building a miltilingual puzzle
 * Return: Instructs not to give any value as output by the main function
 */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
