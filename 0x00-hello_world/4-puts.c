c8cf59c9acc87a14cbde5b23caf3b0df6acbcde8
alexa-coding <alexasoucy123@gmail.com>
2018-06-20 20:14:09 +0000

    removed extra space

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 * main - Prints "Programming is like building a miltilingual puzzle
 * Return: Instructs not to give any value as output by the main function
@@ -7,5 +6,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0); 
+	return (0);
 }
