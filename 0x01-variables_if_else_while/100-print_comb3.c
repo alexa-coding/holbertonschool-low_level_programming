46c62d66d4c0dbba8882380af4684a53fdfbe37d
alexa-coding <alexasoucy123@gmail.com>
2018-06-21 23:43:13 +0000

    print the lowest combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 0-9
+ * Return: Success
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 48; a <= 56; a++)
+	{
+		for (b = a+1; b <= 57; b++)
+		{
+			putchar(a);
+			putchar(b);
+			if (a == 56 && b == 57)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
