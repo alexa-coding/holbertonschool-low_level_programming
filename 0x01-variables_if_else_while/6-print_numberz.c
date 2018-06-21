a050cbf8afcccc53b4d334bfc667c4a2cfbe0521
alexa-coding <alexasoucy123@gmail.com>
2018-06-21 21:17:25 +0000

    added for loop

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 0-9
+ * Return: Success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
