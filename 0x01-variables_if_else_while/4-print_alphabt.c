afedeb61c430ae3953a5c19e6f58ae31b80e3c86
alexa-coding <alexasoucy123@gmail.com>
2018-06-21 20:04:18 +0000

    added for loop and if statement

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ * Return: Success
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (i == 101 || i == 113)
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
