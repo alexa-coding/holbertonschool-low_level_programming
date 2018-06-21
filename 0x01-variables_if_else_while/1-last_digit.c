ab1e5a360048acfee3c1e97aa0d425e6b30568ae
alexa-coding <alexasoucy123@gmail.com>
2018-06-21 18:08:28 +0000

    added variable lastDigit and used if..else statements

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - prints whether the last digit of a number is
+* greater than 5, less than 6 (but not 0), or 0
+* Return: Success
+*/
+
+int main(void)
+{
+	int n;
+	int lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastDigit = n % 10;
+
+	if (lastDigit > 5)
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+	else if (lastDigit < 6 && lastDigit != 0)
+	printf("Last digit of %d is %d and is less than 6 and not zero\n", n
+, lastDigit);
+	else
+	printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+	return (0);
+}
