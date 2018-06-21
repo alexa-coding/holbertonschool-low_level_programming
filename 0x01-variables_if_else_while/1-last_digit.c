fdb46973dd6a902c1ca54982943126ca5ffbaeb4
alexa-coding <alexasoucy123@gmail.com>
2018-06-21 19:31:56 +0000

    changed zero to 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,8 +20,8 @@ int main(void)
 	printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 	else if (lastDigit == 0)
 	printf("Last digit of %d is %d and is zero\n", n, lastDigit);
-	else if (lastDigit < 6 && lastDigit != 0)
-	printf("Last digit of %d is %d and is less than 6 and not zero\n", n
+	else if (lastDigit <  6 && lastDigit != 0)
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n
 , lastDigit);
 	return (0);
 }
