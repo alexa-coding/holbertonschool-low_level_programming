3207d72896be68c3f35f38a25c702ad61fe65d3e
alexa-coding <alexasoucy123@gmail.com>
2018-06-26 17:10:25 +0000

    added description of function

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,17 @@
 #include "holberton.h"
 
 /**
- * _isalpha returns 1 if c is a letter
- */
+ * _isalpha - returns 1 if c is a letter
+ * @c: c contains an integer given by main
+ * Return: 1 if c is a letter, returns 0 otherwise
+*/
 
 int _isalpha(int c)
 {
 	if (c >= 65 && c <= 90)
-		return(1);
+		return (1);
 	else if (c >= 97 && c <= 122)
-		return(1);
+		return (1);
 	else
-		return(0);
+		return (0);
 }
