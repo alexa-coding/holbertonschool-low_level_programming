64c99b2ffb4e4ecd89c1c388ab5d51732dff94cc
alexa-coding <alexasoucy123@gmail.com>
2018-06-26 17:13:09 +0000

    added spaces

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,9 @@
 #include "holberton.h"
 
 /**
- * print_sign - prints whether or not a number is negative, positive, or zero. 
+ * print_sign - prints whether or not a number is negative, positive, or zero.
+ * @n: contains integer assigned by main
+ * Return: 1 if positive, 0 if zero, -1 if other
  */
 
 int print_sign(int n)
@@ -9,14 +11,14 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return(1);
+		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		return(0);
+		return (0);
 	}
 	else
 		_putchar('-');
-	return(-1);
+	return (-1);
 }
