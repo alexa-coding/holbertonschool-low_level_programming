17e64b95fb2ac518546b8ecef484b6c90d6278ca
alexa-coding <alexasoucy123@gmail.com>
2018-06-26 16:15:17 +0000

    removed extra space, added space around = and before (

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 /**
  * main - prints Holberton followed by newline
+ * Return: Success
  */
 
 int main(void)
 {
 	int i;
-        char c[10]="Holberton\n";
+	char c[10] = "Holberton\n";
 
-	for(i = 0; i <= 10; i++)
+	for (i = 0; i <= 10; i++)
 		_putchar(c[i]);
 
-	return(0);
+	return (0);
 }
