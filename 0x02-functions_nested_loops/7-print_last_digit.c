a251f24b414743bbcb0c3bada0c73fae78fccc9b
alexa-coding <alexasoucy123@gmail.com>
2018-06-26 01:16:59 +0000

    used if...else

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - returns last value of a digit
+ */
+
+int print_last_digit(int a)
+{
+	int l = a % 10;
+
+	if (l >= 0)
+	{
+		_putchar(l + '0');
+		return(l);
+	}
+	else
+	{
+		_putchar(-l + '0');
+		return(-l);
+	}
+}
