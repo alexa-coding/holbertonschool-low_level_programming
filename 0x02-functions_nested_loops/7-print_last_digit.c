cd138d4294a00e5ba2450592d5dad71b4e4c07da
alexa-coding <alexasoucy123@gmail.com>
2018-06-26 17:17:43 +0000

    added space before (

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,6 +2,8 @@
 
 /**
  * print_last_digit - returns last value of a digit
+ * @a: contains integer assigned by main
+ * Return: last digit of a
  */
 
 int print_last_digit(int a)
@@ -11,11 +13,11 @@ int print_last_digit(int a)
 	if (l >= 0)
 	{
 		_putchar(l + '0');
-		return(l);
+		return (l);
 	}
 	else
 	{
 		_putchar(-l + '0');
-		return(-l);
+		return (-l);
 	}
 }
