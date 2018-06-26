bad03fe286d0d4a6b0384a7cbe9a327127d268c4
alexa-coding <alexasoucy123@gmail.com>
2018-06-26 17:15:03 +0000

    added parameter description

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,12 +2,14 @@
 
 /**
  * _abs - computes the absolute values of an integer
+ * @q: contains integer assigned by main
+ * Return: absolute value of integer
  */
 
 int _abs(int q)
 {
 	if (q < 0)
-		return(q * -1);
+		return (q * -1);
 	else
-		return(q);
+		return (q);
 }
