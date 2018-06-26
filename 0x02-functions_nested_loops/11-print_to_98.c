e8041be59fb7578435e21e5525f06b6f2d013877
alexa-coding <alexasoucy123@gmail.com>
2018-06-26 19:50:36 +0000

    used for loop and if..else statement

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers form n to 90
+ * @n: contains integer values assigned by main
+ */
+
+void print_to_98(int n)
+{
+	for (n = n; n <= 98; n++)
+	{
+		if (n == 98)
+			printf("%d\n", n);
+		else
+			printf("%d, ", n);
+	}
+}
