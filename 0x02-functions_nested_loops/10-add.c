edacdd88240516e8b2ba907063f57e5f2eb6dc13
alexa-coding <alexasoucy123@gmail.com>
2018-06-26 19:31:34 +0000

    used _putchar

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - returns the sum of two integers
+ * @x: first integer assigned by main
+ * @y: second integer assigned by main
+ * Return: the sum of two integers
+ */
+
+int add(int x, int y)
+{
+	int w = x + y;
+
+	return (w);
+}
