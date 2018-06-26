5c73c8432341632e8e3b5879c007cbeeaf4f290b
alexa-coding <alexasoucy123@gmail.com>
2018-06-26 16:31:47 +0000

    removed extra space and added description for functions print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * main - prints alphabet 10 times
+ * print_alphabet_x10 - prints alphabet 10 times
  */
 
 void print_alphabet_x10(void)
@@ -9,15 +9,14 @@ void print_alphabet_x10(void)
 	int a = 0;
 	char b;
 
-	while(a <=10)
+	while (a <= 10)
 	{
 		b = 'a';
-		while(b <= 'z')
+		while (b <= 'z')
 		{
 			_putchar(b);
 			b++;
 		}
-		
 		_putchar('\n');
 		a++;
 	}
