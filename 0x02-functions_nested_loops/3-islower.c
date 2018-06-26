ec390b26dc41e9b18fca069673bc6f13a6839de3
alexa-coding <alexasoucy123@gmail.com>
2018-06-26 16:39:28 +0000

    added parameter description

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,14 @@
 
 /**
  * _islower - returns 1 if c is lowercase, returns 0 otherwise
+ * @c: contains an integer as its value
+ * Return: returns 1 if value is a lower case letter, returns 0 if otherwise
  */
 
 int _islower(int c)
 {
-	if(c >= 97 && c <= 122)
-		return(1);
+	if (c >= 97 && c <= 122)
+		return (1);
 	else
-		return(0);
+		return (0);
 }
