b88b56c7db02dbfb5123ed3f85d732e3f53b7c35
alexa-coding <alexasoucy123@gmail.com>
2018-06-28 17:44:56 +0000

    multiplied two variables

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: contains integer value set by main
+ * @b: contains integer value set by main
+ * Return: Success
+ */
+
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
