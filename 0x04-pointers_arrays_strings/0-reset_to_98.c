92b6bf291388ee354e5f4871263ee5cc8792a7d0
alexa-coding <alexasoucy123@gmail.com>
2018-07-02 19:16:40 +0000

    0-reset_to_98.c

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,10 @@
 
 /**
 * reset_to_98 - updates the value of a variable using a pointer
+* @*n: pointer that points to an int variable with a  value assigned by 0-main.c
 */
 
 void reset_to_98(int *n)
 {
-
+	*n = 98;
+}
