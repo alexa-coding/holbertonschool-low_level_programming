44dd2cdc760ec55a4cd1d578b3566c17acce8e55
alexa-coding <alexasoucy123@gmail.com>
2018-07-02 19:35:31 +0000

    used while loop

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+  * _strlen - returns the length of a string
+  * @*s: pointer that points to an int variable with a value assigned by 2-main.c
+  */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
