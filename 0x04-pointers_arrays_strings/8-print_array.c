013923b85c6dd8ba25ad75190e6ddde35a7860b8
alexa-coding <alexasoucy123@gmail.com>
2018-07-03 22:27:02 +0000

    used while loops with a nested if..else statement

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * print_array - prints n elements of array
+  * @a: pointer to array
+  * @n: number of elements needed to print the array
+  */
+
+void print_array(int *a, int n)
+{
+	int el = 0;
+
+	while (el != n)
+	{
+		if (el == n - 1)
+			printf("%d", a[el]);
+		else
+			printf("%d, ", a[el]);
+			el++;
+	}
+	printf("\n");
+}
