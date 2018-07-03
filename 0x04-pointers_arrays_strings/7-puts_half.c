6ef1bb8ee44dc5a6ea44225382f2d97e64306eeb
alexa-coding <alexasoucy123@gmail.com>
2018-07-03 21:48:44 +0000

    used for loop, if..else statements

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+  * puts_half - prints the second half of a string
+  * @str: contains a char value assigned by 7-main
+  */
+
+void puts_half(char *str)
+{
+	int length = 0, h1;
+
+	while (str[length] != '\0')
+		length++;
+
+	if (length % 2 == 0)
+		h1 = (length) / 2;
+	else
+		h1 = (length - 1) / 2;
+
+	for (; h1 < length; h1++)
+		_putchar(str[h1]);
+	_putchar('\n');
+}
