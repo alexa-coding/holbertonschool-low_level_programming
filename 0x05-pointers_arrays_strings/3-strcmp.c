9c69b529683485a395d1b7c3c09422e88e10aa3a
alexa-coding <alexasoucy123@gmail.com>
2018-07-06 17:46:48 +0000

    used if..else if.. else statement

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+  * _strcmp - compares two strings
+  * @s1: first string
+  * @s2: second string
+  * Return: integer <, >, or = 0 if s1 is found to be less than, equal to,
+  * or greater than s2
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+	if (*s1 > *s2)
+		return (15);
+	else if (*s1 < *s2)
+		return (-15);
+	else
+		return (0);
+}
