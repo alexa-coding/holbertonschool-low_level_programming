af0bfa0b1a40a790526020fe91dfcaea5034e0d6
alexa-coding <alexasoucy123@gmail.com>
2018-07-06 21:59:32 +0000

    used while and for loop

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+  * *cap_string - capitalizes all words of a string
+  * @s: pointer to string
+  * Return: pointer to the resulting string
+  */
+
+char *cap_string(char *s)
+{
+	int el;
+
+	for (el = 0; s[el] != '\0'; el++)
+	{
+		while (s[el] == '.' || s[el] == ',' || s[el] == ';' || s[el] == '\"'
+|| s[el] == '\?' || s[el] == '!' || s[el] == '(' || s[el] == ')' ||
+s[el] == '{' || s[el] == '}' || s[el] == 32 || s[el] == 9 || s[el] == '\n')
+		{
+			if (s[el + 1] >= 'a' && s[el + 1] <= 'z')
+				s[el + 1] -= 32;
+			break;
+		}
+	}
+
+	return (s);
+}
