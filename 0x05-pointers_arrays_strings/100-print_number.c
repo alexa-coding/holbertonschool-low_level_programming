79bd3d327a5aeb6b8292cfb68fe39bef99d9257e
alexa-coding <alexasoucy123@gmail.com>
2018-07-08 21:50:28 +0000

    100-print_number.c

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <math.h>
+
+/**
+  * print_number - prints a number
+  * @n: variable containing an integer value
+  */
+
+void print_number(int n)
+{
+	int m = 1, num = n, cnt = 0;
+
+	if (num < 0)
+		num = -n;
+
+	while (num > 9)
+	{
+		num /= 10;
+		m *= 10;
+		cnt++;
+	}
+	if (n < 0)
+		_putchar('-');
+
+	while (m > 0)
+	{
+		num = n / m;
+		m /= 10;
+		if (n < 0)
+			_putchar(num % 10 * -1 + '0');
+		else
+			_putchar(num % 10 + '0');
+	}
+}
