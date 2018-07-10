268388752545af12e12beb8c0b8aef1b7a838ec1
alexa-coding <alexasoucy123@gmail.com>
2018-07-10 19:59:18 +0000

    added another if statement

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -10,15 +10,12 @@
 
 char *_strchr(char *s, char c)
 {
-	int i, len;
-
-	for (i = 0; s[i] != '\0'; i++)
-		len++;
-
-	for (i = 0; i < len; s++, i++)
+	for (; *s != '\0'; s++)
 		if (*s == c)
 		{
 			return (s);
 		}
+	if (*s == c)
+		return (s);
 	return (0);
 }
