7059ec8d77090a14ca8a8d8f9c2a0f1a1140cf37
alexa-coding <alexasoucy123@gmail.com>
2018-07-11 20:38:17 +0000

    added description of helper function

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -2,6 +2,8 @@
 
 /**
   * _sqrt_recursion - returns the natural square root of a number
+  * sqrt2 - calculates the square root of a number
+  * @i: contains an integer
   * @n: variable holding integer value
   * Return: natural square root. If n doesn't have a natural square root
   * return -1
@@ -21,5 +23,5 @@ int _sqrt_recursion(int n)
 		return (-1);
 	if (n == 0 || n == 1)
 		return (1);
-	return(sqrt2(n, 1));
+	return (sqrt2(n, 1));
 }
