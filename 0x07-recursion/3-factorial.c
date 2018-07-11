9736dbbd2dc94d8e398e2fc62d95f973a9a3f8c1
alexa-coding <alexasoucy123@gmail.com>
2018-07-11 17:31:10 +0000

    used three if statements

---
+++ 0x07-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+  * factorial - returns teh factorial of a given number
+  * @n: variable containing an integer
+  * Return: factorial of n
+  */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (1);
+	n = n * factorial(n - 1);
+	if (n < 0)
+		n = -n;
+	return(n);
+}
