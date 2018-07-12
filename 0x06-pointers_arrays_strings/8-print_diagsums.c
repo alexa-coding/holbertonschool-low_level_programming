c56dc07d21e6bb80dbac34b04fb2ae91f2db4463
alexa-coding <alexasoucy123@gmail.com>
2018-07-12 03:27:21 +0000

    used for loops and if statements

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+  * print_diagsums - prints the sum of the two diagonals of a square
+  * matrix of integers
+  * @a: pointer to an array
+  * @size: size of the 2D arrays
+  */
+
+void print_diagsums(int *a, int size)
+{
+	int i, j, sum = 0, sum2 = 0;
+
+	for (i = 0, j = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+				sum += *(a + size * i + j);
+
+			if (i + j == size - 1)
+				sum2 += *(a + size * i + j);
+		}
+	}
+	printf("%i, %i\n", sum, sum2);
+}
