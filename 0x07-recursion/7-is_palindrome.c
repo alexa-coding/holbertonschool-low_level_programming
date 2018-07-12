a79720dd7cd65366ce9f8634af07664eb9e55810
alexa-coding <alexasoucy123@gmail.com>
2018-07-12 02:08:58 +0000

    subtracted 1 from len

---
+++ 0x07-recursion/7-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+  * len - determines the length of a string
+  * @s: pointer to a string
+  * Return: returns length of string
+  */
+
+int len(char *s)
+{
+	if (*s)
+		return (1 + len(s + 1));
+	return (0);
+}
+
+/**
+  * pal - determines if a string is a palidrome or not
+  * @a: pointer to beginning or string
+  * @b: pointer to end of string
+  * @temp: holds the initial value of a
+  * Return: 1 if string is palidrome, 0 otherwise
+  */
+
+int pal(char *s, int a, int b)
+{
+	if (a >= b)
+		return (1);
+	if (s[a] == s[b])
+		return (pal(s, a + 1, b - 1));
+	return (0);
+}
+
+int is_palindrome(char *s)
+{
+	return (pal(s, 0, len(s) -  1));
+}
