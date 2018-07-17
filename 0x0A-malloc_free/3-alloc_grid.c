d1409f942637cf3bd138347fd591883b9fe37ab8
alexa-coding <alexasoucy123@gmail.com>
2018-07-17 19:37:02 +0000

    2D array of integers

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+  * **alloc_grid - returns a pointer to a 2 dimensional array of integers
+  * @width: contains int value
+  * @height: contains int value
+  * Return: NULL if failed, NULL if width or height is negative/0, pointer
+  * to array otherwise
+  */
+
+int **alloc_grid(int width, int height)
+{
+	int **a, h, w, i;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	a = malloc(2 * sizeof(int *));
+	for (i = 0; i < height; i++)
+		a[i] = malloc(width * sizeof(int));
+
+	if (a == NULL)
+		return (NULL);
+
+	for (h = 0; h < height; h++)
+		for (w = 0; w < width; w++)
+			a[h][w] = 0;
+	return (a);
+}
