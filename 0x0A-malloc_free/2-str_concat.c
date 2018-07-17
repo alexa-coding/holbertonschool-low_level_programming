ebf630d63a82d46b06a132bcf5e832c2c8d20cbc
alexa-coding <alexasoucy123@gmail.com>
2018-07-17 17:45:12 +0000

    added if statement for when the string is NULL

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -13,6 +13,11 @@ char *str_concat(char *s1, char *s2)
 	char *a;
 	int i, j, len = 0;
 
+	if (s1 == NULL)
+		s1 == "";
+	if (s2 == NULL)
+		s2 == "";
+
 	for (i = 0; s1[i] != 0; len++, i++)
 		;
 	for (i = 0; s2[i] != 0; len++, i++)
