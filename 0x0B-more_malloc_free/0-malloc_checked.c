f0553792d5233d72f875cf13c60f7fc337307c05
alexa-coding <alexasoucy123@gmail.com>
2018-07-19 16:47:42 +0000

    added return description and added ()

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -4,13 +4,14 @@
 /**
   * *malloc_checked - allocates memory using malloc
   * @b: unsigned int
+  * Return: pointer to the allocated memory
   */
 
 void *malloc_checked(unsigned int b)
 {
 	char *res;
 
-	res = malloc(sizeof *res * b);
+	res = malloc(sizeof(*res) * b);
 	if (res == NULL)
 		exit(98);
 	return (res);
