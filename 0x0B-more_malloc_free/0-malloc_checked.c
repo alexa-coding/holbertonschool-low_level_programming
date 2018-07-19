75a902c0fd514297a6d53100461513694de5d524
alexa-coding <alexasoucy123@gmail.com>
2018-07-19 16:43:44 +0000

    allocates memory

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+  * *malloc_checked - allocates memory using malloc
+  * @b: unsigned int
+  */
+
+void *malloc_checked(unsigned int b)
+{
+	char *res;
+
+	res = malloc(sizeof *res * b);
+	if (res == NULL)
+		exit(98);
+	return (res);
+}
