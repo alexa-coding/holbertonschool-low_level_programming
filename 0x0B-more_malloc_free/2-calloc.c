fed569b96af858cbdda82e3c606b4fccecd0318e
alexa-coding <alexasoucy123@gmail.com>
2018-07-19 17:55:04 +0000

    changed int to char

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -10,13 +10,18 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *res;
+	char *res;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	res = malloc(nmemb * sizeof(size));
+	res = malloc(nmemb * size);
 	if (res == NULL)
 		return (NULL);
+	
+	for (i = 0; i < nmemb; i++)
+		res[i] = 0;
+
 	return (res);
 }
