07537e34d8c7a2629cdb8f6fbfd6e977c9459549
alexa-coding <alexasoucy123@gmail.com>
2018-07-19 16:44:37 +0000

    new directory

---
+++ 0x0B-more_malloc_free/holberton.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *malloc_checked(unsigned int b);
+
+#endif
