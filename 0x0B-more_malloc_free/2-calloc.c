d029ea941554678cdf7daf266653f855848d3ec7
alexa-coding <alexasoucy123@gmail.com>
2018-07-19 17:57:57 +0000

    multiplied nmemb by size in my for loop condition

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (res == NULL)
 		return (NULL);
 	
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		res[i] = 0;
 
 	return (res);
