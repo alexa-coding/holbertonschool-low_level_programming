cc8e70ea82beb3ec528f0c0c3f60a194cdcef359
alexa-coding <alexasoucy123@gmail.com>
2018-07-20 00:55:00 +0000

    casted ptr

---
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+  * *_realloc - reallocating memory block using malloc() and free()
+  * @ptr: pointer to the memory previously allocated
+  * @old_size: size, in bytes, of the allocated space for ptr
+  * @new_size: new size, in bytes of the new memory block
+  * Return: 
+  */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *res;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	res = malloc(new_size);
+	if (res == NULL)
+	{
+		free(res);
+		return (NULL);
+	}
+
+	for (i = 0; i < old_size; i++)
+	{
+		res[i] = *((char *)ptr + 1);
+	}
+	return (res);
+}
