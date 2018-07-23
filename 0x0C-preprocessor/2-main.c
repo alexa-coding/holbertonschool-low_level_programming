6792c46bea5f367133b26ef47445773baafbc1ed
alexa-coding <alexasoucy123@gmail.com>
2018-07-23 17:39:18 +0000

    prints file name

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * main - prints file name
+  * Return: 0 if program is successful
+  */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
