e21c916838bf6b5acd72c40b04b153201a9f8d04
alexa-coding <alexasoucy123@gmail.com>
2018-07-24 01:11:14 +0000

    creates a new dog

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * *new_dog - creates a new dog
+  * @name: pointer
+  * @age: float
+  * @owner: pointer
+  * Return: 0 if program is successful
+  */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *a;
+	(void)age;
+
+	a = malloc(sizeof(struct dog));
+	if (a == NULL)
+		return (NULL);
+	a->name = name;
+	a->owner = owner;
+	return (a);
+}
