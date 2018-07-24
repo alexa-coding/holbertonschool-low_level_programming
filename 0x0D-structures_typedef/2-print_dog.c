c99609b5c01edd0f257c122b1f0ee9767cd9e621
alexa-coding <alexasoucy123@gmail.com>
2018-07-24 02:41:51 +0000

    removed if and else

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -15,10 +15,7 @@ void print_dog(struct dog *d)
 		printf("Name: (nill)\n");
 	else
 		printf("Name: %s\n", d->name);
-	if (d->age == 0)
 		printf("Age: (nill)\n");
-	else
-		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner: (nill)\n");
 	else
