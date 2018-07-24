38ef09fe0208bd33de1a0aabf34f78d0bcb29b7b
alexa-coding <alexasoucy123@gmail.com>
2018-07-24 02:43:24 +0000

    added d->

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 		printf("Name: (nill)\n");
 	else
 		printf("Name: %s\n", d->name);
-		printf("Age: (nill)\n");
+		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner: (nill)\n");
 	else
