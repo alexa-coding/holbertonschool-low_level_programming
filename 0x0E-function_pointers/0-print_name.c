e36a898cb830917477474138657d2e0668986eaf
alexa-coding <alexasoucy123@gmail.com>
2018-07-25 17:18:13 +0000

    prints name

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+  * print_name - prints name
+  * @name: pointer to string
+  * @f: pointer to function that prints name
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
