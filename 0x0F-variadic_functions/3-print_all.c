05c55af643a538eff7d5d58061d20a9a7d4b3175
alexa-coding <alexasoucy123@gmail.com>
2018-07-29 05:49:06 +0000

    prints everything

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,69 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints almost anything
+ * @format: list of types of arguments passed to the function
+ * @n: number of strings
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i = 0, len = 0, flag;
+	va_list val;
+	char *var;
+
+	va_start(val, format);
+
+	while (format[len])
+		len++;
+	
+	while (format[i]) /* iterate through parameters and determine types*/
+	{
+		flag = 0;
+
+		switch (format[i])
+		{
+			case 'c':
+			{
+				printf("%c", va_arg(val, int));
+				break;
+			}
+			case 'i':
+			{
+				printf("%i", va_arg(val, int));
+				break;
+			}
+			case 'f':
+			{
+				printf("%f", va_arg(val, double));
+				break;
+			}
+			case 's':
+			{
+				var = va_arg(val, char *);
+				if (var)
+				{
+					printf("%s", var);
+					break;
+				}
+				else
+				{
+					printf("(nil)");
+					break;
+				}
+			}
+			default:
+			{
+				flag = 1;
+				break;
+			}
+		}
+		if (i < len - 1 && flag == 0)
+			printf(", ");
+		i++;
+		}
+	printf("\n");
+}
