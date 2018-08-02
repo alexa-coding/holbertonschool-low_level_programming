c4f7c5f6affa13d37ea6fd978e651e8640b00f4e
alexa-coding <alexasoucy123@gmail.com>
2018-08-02 21:24:10 +0000

    file executes before main

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+  * pre_main - prints a string
+  */
+
+void __attribute__ ((constructor)) pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
