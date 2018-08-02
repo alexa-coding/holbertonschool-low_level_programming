dc2b04efc8c4c9aa7e2831b3fc667b492a29ab03
alexa-coding <alexasoucy123@gmail.com>
2018-08-02 20:27:19 +0000

    removed trailing white space

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -15,12 +15,12 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new;
 	list_t *search;
 	int i;
-	
+
 	search = *head;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
-		return(NULL);
+		return (NULL);
 	new->str = strdup(str);
 
 	for (i = 0; str[i] != 0; i++)
