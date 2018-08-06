468f28dad7c5db70d7a0c9331e89bb2ed5bcc138
alexa-coding <alexasoucy123@gmail.com>
2018-08-06 17:57:56 +0000

    added check if head is NULL

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
+	if (!head)
+		return (0);
+
 	for (i = 0; i < index; i++)
+	{
 		head = head->next;
+		if (!head)
+			return (0);
+	}
 	return (head);
 }
