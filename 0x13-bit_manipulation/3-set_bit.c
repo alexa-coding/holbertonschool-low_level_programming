6430255a2472a70ebe22cb6be46d6384378d8479
alexa-coding <alexasoucy123@gmail.com>
2018-08-09 20:55:10 +0000

    sets the value of a specific index to 1

---
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * set_bit -  sets the value of a bit to 1 at a given index
+  * @n: pointer to an integer
+  * @index: where the desired bit is located
+  * Return: 1 if program is successful, -1 otherwise
+  */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i;
+	unsigned long int temp = *n, temp2;
+
+	for (temp2 = 1, i = 0; i < index; i++)
+		temp2 <<= 1;
+
+	*n = temp | temp2;
+	return (1);
+}
