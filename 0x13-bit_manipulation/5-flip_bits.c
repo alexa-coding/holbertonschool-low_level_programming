93d3db9d74423298a119697d9961cd6be2f1ade9
alexa-coding <alexasoucy123@gmail.com>
2018-08-09 23:08:25 +0000

    removed if statements

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -13,11 +13,6 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int xor, count = 0;
 
-	if (n > sizeof(n) * 8)
-		return (-1);
-	if (m > sizeof(n) * 8)
-		return (-1);
-
 	xor = n ^ m;
 
 	while (xor != 0)
